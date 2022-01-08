bfce3c70f6021221e76ecbfee0a1f395be3e7f10
AgomohC <chinaemerem98@yahoo.com>
2022-01-08 20:53:16 +0100

    added  userschema

--- models/User.js
+++ models/User.js
@@ -1,13 +1,44 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const UserSchema = mongoose.Schema({
-   //  title: {
-   //     type: String,
-   //     unique: true,
-   //  },
-   //  commentCount: {
-   //     type: Number,
-   //     default: 0,
-   //  },
+   name: {
+      type: String,
+      required: [true, "please enter name"],
+   },
+   email: {
+      type: String,
+      required: [true, "please enter email"],
+   },
+   username: {
+      type: String,
+      required: [true, "please enter username"],
+   },
+   cartItems: [
+      {
+         type: mongoose.Types.ObjectId,
+         ref: "Cart",
+      },
+   ],
+   password: {
+      type: String,
+      required: [true, "please enter password"],
+   },
 });
+
+//hash password before saving to the dbs
+UserSchema.pre("save", async function () {
+   this.delete_password = await bcrypt.hash(this.delete_password, 12);
+});
+
+// compare password with hash in db
+ThreadSchema.methods.comparePasswords = async function (candidatePassword) {
+   const isMatch = await bcrypt.compare(
+      candidatePassword,
+      this.delete_password
+   );
+   return isMatch;
+};
+
 const User = mongoose.model("User", UserSchema);
+
 module.exports = User;
