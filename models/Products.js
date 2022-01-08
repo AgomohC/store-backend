3f73b5a178bd0381ab014631296c0ec62b906ada
AgomohC <chinaemerem98@yahoo.com>
2022-01-08 20:57:38 +0100

    added product schema

--- models/Products.js
+++ models/Products.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose");
 const ProductSchema = mongoose.Schema({
-   //  title: {
-   //     type: String,
-   //     unique: true,
-   //  },
+   title: {
+      type: String,
+   },
+   price: {
+      type: Number,
+   },
+   description: {
+      type: String,
+   },
+   category: {
+      type: String,
+   },
+   image: {
+      type: String,
+   },
    //  commentCount: {
    //     type: Number,
    //     default: 0,
